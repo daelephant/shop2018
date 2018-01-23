2e6c939e32939e1dc0bf3192f62532d40b30d450
elephant2 <2224168716@qq.com>
2018-01-23 18:11:29 +0800

    0123

--- Application/Admin/Model/CategoryModel.class.php
+++ Application/Admin/Model/CategoryModel.class.php
@@ -35,4 +35,52 @@ class CategoryModel extends Model
         }
         return $_ret;
     }
+
+    public function getTree(){
+        $data = $this->select();
+        return $this->_getTree($data);
+    }
+    private function _getTree($data,$parent_id=0,$level=0){
+        static $_ret = array();
+        foreach ($data as $k => $v){
+            if($v['parent_id'] == $parent_id){
+                $v['level'] = $level;//用来标记这个分类是第几级的
+                $_ret[] = $v;
+                //找子分类
+                $this->_getTree($data,$v['id'],$level+1);
+            }
+        }
+        return $_ret;
+    }
+    /*删除方法一：*/
+    protected function _before_delete($options)
+    {
+        //先找出所有子分类的ID
+        $children = $this->getChildren($options['where']['id']);
+        //var_dump($children);exit();
+        if($children){
+            $children = implode(',',$children);//把数组转化成字符串。以，分割
+            //删除这些子分类
+            //避免before delete死循环
+            //说明这里必须生成父类模型然后调用delete，因为如果使用$this调用delete那么在delete之前又会调用$this->>_before_delete
+            //这样就死循环了。用父类的delete就会在delete之前调用父类的before_delete和这个_before_delete没关系，就不会死循环了！！！
+            $model = new \Think\Model();//实例化父类model
+            $model->table('__CATEGORY__')->delete($children);//简化数据表前缀的传入,TP自带
+        }
+    }
+    /*删除方法二：*/
+    /******修改源$option,把所有子分类的id也加进来，这样tp会一起删除掉**********/
+    //protected function _before_delete(&$options)
+    //{
+    //        //先找出所有子分类的ID
+    //        $children = $this->getChildren($options['where']['id']);
+    //        var_dump($children);exit();
+    //        $children[] = $options['where']['id'];//把要删除的id添加到子类的数组中，方便下一步整体删除
+    //        //再填回拼装成TP识别的数据形式：$options['where']['id']
+    //        $options['where']['id'] = array(
+    //            0=>'IN',
+    //            1=>implode(',',$children)
+    //        );
+    //}
+
 }
