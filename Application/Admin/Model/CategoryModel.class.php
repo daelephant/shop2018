cdc58c779a98e7b8ad8f43eb83798115bcaedd45
family <2224168716@qq.com>
2018-03-21 07:26:48 +0800

    0321am

--- Application/Admin/Model/CategoryModel.class.php
+++ Application/Admin/Model/CategoryModel.class.php
@@ -88,28 +88,37 @@ class CategoryModel extends Model
      *
      */
     public function getNavData(){
-        //取出所有的分类
-        $all = $this->select();
-        $ret = array();//一维数组是顶级分类
-        //循环出所有的分类找出顶级分类
-        foreach ($all as $k=>$v){
-            if ($v['parent_id'] == 0){
-                //循环出所有的分类，找出这个顶级分类的子分类
-                foreach ($all as $k1=>$v1){
-                    if ($v1['parent_id'] == $v['id']){
-                        //循环所有的分类找出这个二级分类的子分类
-                        foreach ($all as $k2=>$v2){
-                            if ($v2['parent_id'] == $v1['id']){
-                                $v1['children'][] = $v2;
+        //先从缓存中取出数据
+        $catData = S('catData');
+        //判断如果没有缓存或者缓存过期就重新构造数组
+        if(!$catData){
+            //取出所有的分类
+            $all = $this->select();
+            $ret = array();//一维数组是顶级分类
+            //循环出所有的分类找出顶级分类
+            foreach ($all as $k=>$v){
+                if ($v['parent_id'] == 0){
+                    //循环出所有的分类，找出这个顶级分类的子分类
+                    foreach ($all as $k1=>$v1){
+                        if ($v1['parent_id'] == $v['id']){
+                            //循环所有的分类找出这个二级分类的子分类
+                            foreach ($all as $k2=>$v2){
+                                if ($v2['parent_id'] == $v1['id']){
+                                    $v1['children'][] = $v2;
+                                }
                             }
+                            $v['children'][] = $v1;
                         }
-                        $v['children'][] = $v1;
                     }
+                    $ret[] = $v;
                 }
-                $ret[] = $v;
             }
+            //把数组缓存一天
+            S('catData',$ret,56400);
+            return $ret;
+        }else{
+            return $catData;//有缓存直接返回缓存数据
         }
-        return $ret;
     }
 
 }
