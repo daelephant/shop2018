4099972f1eeefaacca69b6727cb538774e29add2
elephant2 <2224168716@qq.com>
2018-03-20 15:50:36 +0800

    0320pm

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,7 +2,26 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+    //首页
     public function index(){
+        //设置页面信息
+        $this->assign(array(
+            '_show_nav' => 1,
+            '_page_title' => '首页',
+            '_page_keywords' => '首页',
+            '_page_description' => '首页',
+        ));
+        $this->display();
+    }
+    //商品详情页
+    public function goods(){
+        //设置页面信息
+        $this->assign(array(
+            '_show_nav' => 0,
+            '_page_title' => '商品详情页',
+            '_page_keywords' => '特价商品详情页...',
+            '_page_description' => '特卖商品详情页...',
+        ));
         $this->display();
     }
 }
