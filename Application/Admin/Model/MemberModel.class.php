bc47556a36916258167c411b10166b7f4e4d8389
elephant2 <2224168716@qq.com>
2018-03-27 09:47:08 +0800

    0327am

--- Application/Admin/Model/MemberModel.class.php
+++ Application/Admin/Model/MemberModel.class.php
@@ -4,15 +4,16 @@ use Think\Model;
 class MemberModel extends Model
 {
     //添加和修改会员时使用的表单验证规则
-	protected $insertFields = array('username','password','cpassword','chkcode');
+	protected $insertFields = array('username','password','cpassword','chkcode','must_click');
 	protected $updateFields = array('id','username','password','cpassword');
 	//注册时用到
 	protected $_validate = array(
+		array('must_click', 'require', '必须同意注册协议！', 1, 'regex', 3),
 		array('username', 'require', '用户名不能为空！', 1, 'regex', 3),
 		array('username', '1,30', '用户名的值最长不能超过 30 个字符！', 1, 'length', 3),
 		//第六个参数：代表规则什么时候生效，1：仅添加时生效，2：修改时生效，3：所有情况生效
 		array('password', 'require', '密码不能为空！', 1, 'regex', 1),
-		//array('password', '1,32', '密码的值最长不能超过 32 个字符！', 1, 'length', 3),
+		array('password', '6,20', '密码的值最长不能低于6 或超过 20 个字符！', 1, 'length', 3),
 		array('username', '', '用户名已经存在！', 1, 'unique', 3),
 		array('cpassword','password','两次密码输入不一致!',1,'confirm',3),
         array('chkcode','require','验证码不能为空!!!',1),
@@ -20,11 +21,11 @@ class MemberModel extends Model
 	);
 	//登录时用到
 	//为登录的表单定义一个验证规则,需要时公开的属性才可以调用
-    public $_login_validata = array(
+    public $_login_validate = array(
         array('username','require','用户名不能为空！！',1),
         array('password','require','密码不能为空！！',1),
         array('chkcode','require','验证码不能为空!!!',1),
-        array('chkcode','check_verify','验证错误',1,'callback'),
+        array('chkcode','check_verify','验证码验证错误',1,'callback'),
     );
     //验证 验证码是否正确
     function check_verify($code,$id=''){
@@ -54,8 +55,12 @@ class MemberModel extends Model
             return FALSE;
         }
     }
+    public function _before_insert(&$data, $options)
+    {
+        $data['password'] = md5($data['password']);
+    }
 
-	public function logout(){
+    public function logout(){
         session(null);
     }
 	/************************************ 其他方法 ********************************************/
--- Application/Home/Controller/MemberController.class.php
+++ Application/Home/Controller/MemberController.class.php
@@ -3,20 +3,70 @@ namespace Home\Controller;
 use Think\Controller;
 class MemberController extends Controller {
 
-    //首页
-    public function index(){
-       
+    public function ajaxChkLogin(){
+        if(session('m_id'))
+            echo json_encode(array(
+               'login' => 1,
+               'username' => session('m_username'),
+            ));
+        else
+            echo json_encode(array(
+               'login' => 0,
+            ));
+    }
+    //制作验证码
+    public function chkcode(){
+        $Verify = new \Think\Verify(array(
+            'fontSize'=>30,//验证码字体大小
+            'length'=>2,//验证码长度
+            'useNoise'=>TRUE,//开启or关闭验证码杂点
+        ));
+        $Verify->entry();
+    }
+    //登录
+    public function login(){
+       if(IS_POST){
+           $model = D('Admin/Member');
+           if ($model->validate($model->_login_validate)->create()){
+               if($model->login()){
+                   $this->success('登录成功！',U('/'));
+                   exit;
+               }
+           }
+           $this->error($model->getError());
+       }
+       //设置页面信息
         $this->assign(array(
-            'goods1' => $goods1,
-            'goods2' => $goods2,
-            'goods3' => $goods3,
-            'goods4' => $goods4,
-            'floorData' => $floorData,
-            '_show_nav' => 1,
-            '_page_title' => '首页',
-            '_page_keywords' => '首页',
-            '_page_description' => '首页',
+            '_page_title' => '登录',
+            '_page_keywords' => '登录',
+            '_page_description' => '登录',
         ));
         $this->display();
     }
+
+    //注册
+    public function regist(){
+        if(IS_POST){
+            $model = D('Admin/Member');
+            if ($model->create(I('post.',1))){
+                if($model->add()){
+                    $this->success('注册成功！',U('login'));
+                    exit;
+                }
+            }
+            $this->error($model->getError());
+        }
+        $this->assign(array(
+            '_page_title' => '注册',
+            '_page_keywords' => '注册',
+            '_page_description' => '注册',
+        ));
+        $this->display();
+    }
+    public function logout(){
+        $model = D('Admin/Member');
+        $model->logout();
+        redirect('/');
+    }
+
 }
