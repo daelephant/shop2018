27dd3a5926fafce23deee34ff2115192b20b5256
family <2224168716@qq.com>
2018-04-06 22:00:51 +0800

    0406ev for home search

--- Application/Home/Controller/SearchController.class.php
+++ Application/Home/Controller/SearchController.class.php
@@ -4,13 +4,18 @@ class SearchController extends NavController {
     //分类搜索
     public function cat_search(){
         $catId = I('get.cat_id');
-
+        //取出筛选条件
         $catModel = D('Admin/Category');
         $searchFilter = $catModel->getSearchConditionByCatId($catId);
 
+        //取出商品和翻页
+        $goodsModel = D('Admin/Goods');
+        $data = $goodsModel->cat_search($catId);
 
         //设置页面信息
         $this->assign(array(
+            'page' => $data['page'],
+            'data' => $data['data'],
             'searchFilter' => $searchFilter,
             '_page_title' => '分类搜索',
             '_page_keywords' => '分类搜索...',
